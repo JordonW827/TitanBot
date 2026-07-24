aaec35340c5e6dae04802cc9f5302c12732e11ea
JordonW827 <jordonwang827@gmail.com>
2026-07-23 20:49:22 -0700

    Update crime.js

--- src/commands/Economy/crime.js
+++ src/commands/Economy/crime.js
@@ -4,8 +4,8 @@ import { getEconomyData, setEconomyData } from '../../utils/economy.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const CRIME_COOLDOWN = 60 * 60 * 5;
-const JAIL_TIME = 2 * 60 * 60 * 100;
+const CRIME_COOLDOWN = 60 * 60 * 1000;
+const JAIL_TIME = 2 * 60 * 60 * 1000;
 const FINE_RATE = 0.2;
 
 const CRIME_TYPES = [
