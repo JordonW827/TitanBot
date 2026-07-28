42ca8af0ea8605739bc9f5d7369b05d48edb5056
JordonW827 <jordonwang827@gmail.com>
2026-07-27 19:49:38 -0700

    Update gamble.js

--- src/commands/Economy/gamble.js
+++ src/commands/Economy/gamble.js
@@ -6,7 +6,7 @@ import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const BASE_WIN_CHANCE = 0.4;
 const CLOVER_WIN_BONUS = 0.2;
-const CHARM_WIN_BONUS = 0.5;
+const CHARM_WIN_BONUS = 0.6;
 const PAYOUT_MULTIPLIER = 2.0;
 const GAMBLE_COOLDOWN = 1 * 15 * 1000;
 
