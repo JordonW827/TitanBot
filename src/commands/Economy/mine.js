c0e0f0618af172a94bd977a50701d0855e0b59df
JordonW827 <jordonwang827@gmail.com>
2026-07-28 18:12:27 -0700

    Update mine.js

--- src/commands/Economy/mine.js
+++ src/commands/Economy/mine.js
@@ -5,7 +5,7 @@ import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHan
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const MINE_COOLDOWN = 1 * 10 * 1000;
-const BASE_MIN_REWARD = 400;
+const BASE_MIN_REWARD = 1;
 const BASE_MAX_REWARD = 1200;
 const PICKAXE_MULTIPLIER = 1.2;
 const DIAMOND_PICKAXE_MULTIPLIER = 2.0;
