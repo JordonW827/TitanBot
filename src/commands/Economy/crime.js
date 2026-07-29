1954bc633d4cb35fc7812e2f4d46ed03c182f745
JordonW827 <jordonwang827@gmail.com>
2026-07-28 17:58:17 -0700

    Update crime.js

--- src/commands/Economy/crime.js
+++ src/commands/Economy/crime.js
@@ -4,16 +4,16 @@ import { getEconomyData, setEconomyData } from '../../utils/economy.js';
 import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHandler.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const CRIME_COOLDOWN = 60 * 60 * 1000;
-const JAIL_TIME = 2 * 60 * 60 * 1000;
+const CRIME_COOLDOWN = 1 * 60 * 1000;
+const JAIL_TIME = 60 * 60 * 1000;
 const FINE_RATE = 0.2;
 
 const CRIME_TYPES = [
-    { name: "Pickpocketing", min: 100, max: 500, risk: 0.3 },
-    { name: "Burglary", min: 300, max: 1000, risk: 0.4 },
-    { name: "Bank Heist", min: 1000, max: 5000, risk: 0.6 },
-    { name: "Art Theft", min: 2000, max: 10000, risk: 0.7 },
-    { name: "Cybercrime", min: 5000, max: 20000, risk: 0.8 },
+    { name: "Pickpocketing", min: 100, max: 5000, risk: 0.3 },
+    { name: "Burglary", min: 300, max: 10000, risk: 0.4 },
+    { name: "Bank Heist", min: 1000, max: 50000, risk: 0.6 },
+    { name: "Art Theft", min: 2000, max: 100000, risk: 0.7 },
+    { name: "Cybercrime", min: 5000, max: 200000, risk: 0.8 },
 ];
 
 export default {
